ad776aff6643d3859f01195b8b10ebb0db34e558
haidul12 <haidulmahfud12@gmail.com>
2025-07-01 16:13:34 +0800

    Add files via upload

---
+++ TUGAS KE 7.py
@@ -0,0 +1,9 @@
+Meminta input dari pengguna
+domain = input("Masukkan nama domain anda: ")
+
+# Memisahkan nama dan domain
+nama, domain = domain.split('.')
+
+# Menampilkan hasil
+print("Nama domain anda:", haidul)
+print("Domain yang anda gunakan:",domain
